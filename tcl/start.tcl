cc1539ec7daa5f6b538e5928c84eca0e4d920a0c
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2018-02-16 09:30:08 +0000

    Version bump for 4.19

--- tcl/start.tcl
+++ tcl/start.tcl
@@ -12,8 +12,8 @@
 # The next line restarts using tkscid: \
 exec "`dirname "$0"`"/tkscid "$0" "$@"
 
-set scidVersion 4.18
-set scidVersionDate {Jul 1, 2017}
+set scidVersion 4.19
+set scidVersionDate {Mar 15, 2018}
 
 package require Tcl 8.5
 package require Tk  8.5
@@ -1888,7 +1888,7 @@ proc ::splash::add {text {tag {indent}}} {
 ::splash::add "$::scidName $::scidVersion ($::scidVersionDate)"
 ::splash::add "http://scidvspc.sourceforge.net"
 ::splash::add ""
-::splash::add "(C) Steven Atkinson (stevenaaus@yahoo.com) 2008-2017"
+::splash::add "(C) Steven Atkinson (stevenaaus@yahoo.com) 2008-2018"
 ::splash::add "(C) Pascal Georges 2006-2008"
 ::splash::add "(C) Shane Hudson 1999-2004"
 ::splash::add "(C) Gregor Cramer, Fulvio Benini and others."
