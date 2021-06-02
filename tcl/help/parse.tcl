b753ea220af8245d523911f4f103199df0199809
stevenaaus <stevenaaus@b49c361e-7f30-453a-be38-6c4a2cbfac04>
2021-06-02 23:28:50 +0000

    4.22 version bump.

--- tcl/help/parse.tcl
+++ tcl/help/parse.tcl
@@ -10,8 +10,8 @@
 exec tclsh "$0" "$@"
 
 set ::scidName {Scid vs. PC}
-set ::scidVersion 4.21
-set ::scidVersionDate {Jan 7, 2020}
+set ::scidVersion 4.22
+set ::scidVersionDate {Apr 7, 2021}
 source help.tcl
 
 file mkdir doc
